33cf57df8587622eaa0b71e811ee87ea03101dae
bekauz <bekauz@protonmail.com>
2022-03-17 18:35:44 +0100

    level 15 progress

--- test/15_NaughtCoin/15-naughtcoin.ts
+++ test/15_NaughtCoin/15-naughtcoin.ts
@@ -4,37 +4,30 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { getNewInstance, submitInstance } from "../utils";
 
-const LEVEL_ADDRESS = "0x9b261b23cE149422DE75907C6ac0C30cEc4e652A";
+const LEVEL_ADDRESS = "0x096bb5e93a204BfD701502EB6EF266a950217218";
 
 let owner: SignerWithAddress;
 let attacker: SignerWithAddress;
 let txn: any;
 let contract: Contract;
-let attackContract: Contract;
 
-describe("GatekeeperOne", () => {
+describe.only("NaughtCoin", () => {
 
     beforeEach(async () => {
         [owner, attacker] = await ethers.getSigners();
 
-        const contractFactory = await ethers.getContractFactory("GatekeeperOne");
+        const contractFactory = await ethers.getContractFactory("NaughtCoin");
         const challengeAddr = await getNewInstance(LEVEL_ADDRESS);
         contract = await contractFactory.attach(challengeAddr);
-
-        const attackContractFactory = await ethers.getContractFactory("GatekeeperOneAttack");
-        attackContract = await attackContractFactory.deploy(contract.address);
     });
 
     it("Should solve the challenge", async function () {
         expect(await submitInstance(contract.address)).to.be.false;
-        
-        let gasAmount = 8191 * 5;
+        await contract.transfer(owner.address, 100);
 
-        let gateKey = ethers.utils.keccak256(owner.address);
-
-        txn = await attackContract.attack(gateKey, gasAmount);
-        await txn.wait();
-
+        const initialBalance = await contract.balanceOf(owner.address);
+        console.log(`initial balance: ${initialBalance}`);
+      
         expect(await submitInstance(contract.address), "level is not complete").to.be.true;
     });
 });
